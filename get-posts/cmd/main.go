ff98eb6dad4c6e6f1f7832c1ca843d4502306bb3
Tommy Calvy <tommycalvy@Tommys-MacBook-Pro.local>
2022-10-25 01:19:21 -0500

    fake population center around point

--- get-posts/cmd/main.go
+++ get-posts/cmd/main.go
@@ -1,9 +1,96 @@
 package main
 
 import (
-	"github.com/tommycalvy/forefinder/get-posts/fake"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+
+	fake "github.com/tommycalvy/forefinder/get-posts"
 )
 
+func drawCircle(img draw.Image, x0, y0, r int, c color.Color) {
+    x, y, dx, dy := r-1, 0, 1, 1
+    err := dx - (r * 2)
+
+    for x > y {
+        img.Set(x0+x, y0+y, c)
+        img.Set(x0+y, y0+x, c)
+        img.Set(x0-y, y0+x, c)
+        img.Set(x0-x, y0+y, c)
+        img.Set(x0-x, y0-y, c)
+        img.Set(x0-y, y0-x, c)
+        img.Set(x0+y, y0-x, c)
+        img.Set(x0+x, y0-y, c)
+
+        if err <= 0 {
+            y++
+            err += dy
+            dy += 2
+        }
+        if err > 0 {
+            x--
+            dx += 2
+            err += dx - (r * 2)
+        }
+    }
+}
+
+func setBackgroundColor(img *image.RGBA, c color.Color) {
+	b := img.Bounds()
+
+    for y := b.Min.Y; y < b.Max.Y; y++ {
+        for x := b.Min.X; x < b.Max.X; x++ {
+            img.Set(x, y, c)
+        }
+    }
+}
+
 func main() {
-	fake.GenerateData(100)
+	width := 2048
+	height := 2048
+
+	pop := 2000
+	
+
+	f, err := os.Create("population-test.png")
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	defer f.Close()
+	rect := image.Rect(0, 0, width, height)
+	poptestimg := image.NewRGBA(rect)
+	setBackgroundColor(poptestimg, color.White)
+	drawCircle(poptestimg, 1024, 1024, 500, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 900, color.Black)
+
+	//drawCircle(poptestimg, 1024, 1024, 800, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 700, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 600, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 500, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 400, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 300, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 200, color.Black)
+	//drawCircle(poptestimg, 1024, 1024, 100, color.Black)
+
+
+
+
+	for i := 0; i < pop; i++ {
+		r := fake.FakeDistanceFromCityV6(500)
+		a := rand.Float64() * 360
+		x := int(r * math.Cos(a)) + 1024
+		y := int(r * math.Sin(a)) + 1024
+		drawCircle(poptestimg, x, y, 3, color.RGBA{255, 0, 0, 255})
+	}
+	
+	err = png.Encode(f, poptestimg)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	log.Printf("Success!")
 }
--- get-posts/generatedata.go
+++ get-posts/generatedata.go
@@ -11,15 +11,46 @@ func fakelikes() int {
 	return likes
 }
 
-func fakedistancefromcity(radius float64) float64 {
+func FakeDistanceFromCityV1(radius float64) float64 {
 	randnum := rand.Float64() * 99.173 + 0.827   // So randnum is between [0.827, 100]
 	dist := radius * (1/math.Sqrt(randnum) - 0.1)
 	return dist
 }
 
+func FakeDistanceFromCityV2(radius float64) float64 {
+	randnum := rand.Float64() * 9.091 + 0.909   // So randnum is between [0.827, 100]
+	dist := radius * (1/randnum - 0.1)
+	return dist
+}
+
+func FakeDistanceFromCityV3(radius float64) float64 {
+	randnum := rand.Float64() * 15.36 + 0.64   // So randnum is between [0.827, 100]
+	dist := radius * (1/math.Sqrt(randnum) - 0.25)
+	return dist
+}
+
+func FakeDistanceFromCityV4(radius float64) float64 {
+	randnum := rand.Float64() * 8.4375 + 0.5625   // So randnum is between [0.827, 100]
+	dist := radius * (1/math.Sqrt(randnum) - 0.333)
+	return dist
+}
+
+func FakeDistanceFromCityV5(radius float64) float64 {
+	randnum := rand.Float64() * 3.5555 + 0.4445   // So randnum is between [0.827, 100]
+	dist := radius * (1/math.Sqrt(randnum) - 0.5)
+	return dist
+}
+
+// Looks the best
+func FakeDistanceFromCityV6(radius float64) float64 {
+	randnum := rand.Float64() * 0.75 + 0.25   // So randnum is between [0.827, 100]
+	dist := radius * (1/math.Sqrt(randnum) - 1)
+	return dist
+}
+
 func GenerateData(num int) {
 	for i:=0; i < num; i++ {
-		dist := fakedistancefromcity(10)
+		dist := FakeDistanceFromCityV1(10)
 		fmt.Println(dist)
 	}
 }
